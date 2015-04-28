709c602a488a55fa99af88d7c2e061c289c8e2e6
lq <lq@le1.ca>
2015-04-28 14:47:44 -0600

    update demo

--- main/main.c
+++ main/main.c
@@ -1,41 +1,11 @@
 #include <stdio.h>
-#include <dlfcn.h>
-#include "foo-mod.h"
+#include "module_table.h"
+#include "module_manager.h"
 
 int main(int argc, char **argv){
-    void *foo_mod_hdl;
-    void (*foo_set_f)(const char s[]);
-    void (*foo_hello_f)();
-    char *err;
-
-    // load foo-mod library
-    if(!(foo_mod_hdl = dlopen("./modules/foo-mod.so", RTLD_LAZY))){
-        fprintf(stderr, "Error loading module: %s\n", dlerror());
-        return 0;
-    }
-    
-    // get reference to foo_set function
-    foo_set_f = dlsym(foo_mod_hdl, "foo_set");
-    if((err = dlerror()) != NULL){
-        fprintf(stderr, "Error referencing function: %s\n", err);
-        return 0;
-    }
-    
-    // get reference to foo_hello function
-    foo_hello_f = dlsym(foo_mod_hdl, "foo_hello");
-    if((err = dlerror()) != NULL){
-        fprintf(stderr, "Error referencing function: %s\n", err);
-        return 0;
-    }
-
-    // do the thing
-    (*foo_hello_f)();
-    (*foo_set_f)("foo bar");
-    (*foo_hello_f)();
-
-
-    // release the library
-    dlclose(foo_mod_hdl);
-
+    module_init();
+    module_load("./modules/foo-mod.so");
+    module_call("foo_hello", 0);
+    module_destroy();
     return 0;
 }
